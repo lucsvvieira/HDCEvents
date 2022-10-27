30f1df2ce2d62d12cd91832106a6ce975cac98f8
Lucas Vieira <vieira07lucas@gmail.com>
2022-10-27 16:39:20 -0300

    Salvando JSON no banco de dados

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -27,6 +27,7 @@ class EventController extends Controller
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
+        $event->items = $request->items;
 
         // Image Upload
 
