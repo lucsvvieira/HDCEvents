d1bbabe0cb85c0ec3d9135e5720a23f3e7f82015
Lucas Vieira <vieira07lucas@gmail.com>
2022-10-31 12:28:11 -0300

    Removendo presença do evento e conclusão do projeto

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -73,9 +73,24 @@ class EventController extends Controller
 
         $event = Event::findOrFail($id);
 
+        $user = auth()->user();
+        $hasUserJoined = false;
+
+        if($user) {
+
+            $userEvents = $user->eventsAsParticipant->toArray();
+
+            foreach($userEvents as $userEvent) {
+                if($userEvent['id'] == $id) {
+                    $hasUserJoined = true;
+                }
+            }
+
+        }
+
         $eventOwner = User::where('id', $event->user_id)->first()->toArray();
 
-        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner, 'hasUserJoined' => $hasUserJoined]);
 
     }
 
@@ -144,11 +159,23 @@ class EventController extends Controller
         
         $user = auth()->user();
 
-        $user->eventsAsParticipant->attach($id);
+        $user->eventsAsParticipant()->attach($id);
 
         $event = Event::findOrFail($id);
 
         return redirect('/dashboard')->with('msg', 'Presença no evento ' . $event->title . 'confirmada com sucesso!');
+
+    }
+
+    public function leaveEvent($id) {
+
+        $user = auth()->user();
+
+        $user->eventsAsParticipant()->detach($id);
+
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', 'Sua presença no evento ' . $event->title . ' foi realizada com sucesso!');
 
     }
 
