f94e6b0fdb1199aa19f7ca7e853c262350bb1029
Lucas Vieira <vieira07lucas@gmail.com>
2022-10-31 11:18:17 -0300

    Relação many to many

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,6 @@ Route::get('/contact', function () {
 
 Route::get('/dashboard', [EventController::class, 'dashboard'])->middleware('auth');
 
+Route::post('/events/join/{id}', [EventController::class, 'joinEvent'])->middleware('auth');
+
 
